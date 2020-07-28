d6d4a025dc84e01caf0fd937bde5debff118600c
Gil Fewster <hello@gilfewster.com>
2020-07-29 09:29:14 +1000

    small change preparing for improved block text handling

--- studio/schemas/vines/vine.js
+++ studio/schemas/vines/vine.js
@@ -1,3 +1,4 @@
+import { GiLinkedRings } from 'react-icons/gi';
 import { IoMdApps } from 'react-icons/io';
 
 export default {
@@ -9,51 +10,51 @@ export default {
     select: {
       title: 'title',
       subtitle: 'category',
-      image: 'hero.image'
+      image: 'hero.image',
     },
     prepare(selection) {
       const { title, subtitle, image } = selection;
       return {
         title,
         subtitle,
-        media: image || IoMdApps
+        media: image || IoMdApps,
       };
-    }
+    },
   },
   fields: [
     {
       name: 'title',
       title: 'Title',
       type: 'string',
-      validation: Rule => Rule.required()
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'subtitle',
       title: 'Subtitle',
-      type: 'string'
+      type: 'string',
     },
     {
       name: 'author',
       title: 'Author',
       type: 'reference',
       to: {
-        type: 'royalsPerson'
-      }
+        type: 'royalsPerson',
+      },
     },
     {
       name: 'category',
       title: 'Category',
       type: 'string',
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
       options: {
         list: [
           { title: 'Page', value: 'page' },
           { title: 'Article', value: 'article' },
           { title: 'Work', value: 'work' },
-          { title: 'Other', value: '' }
+          { title: 'Other', value: '' },
         ],
-        layout: 'radio'
-      }
+        layout: 'radio',
+      },
     },
     {
       title: 'Slug',
@@ -62,19 +63,50 @@ export default {
       description:
         'A unique identifier for this record, used for URL generation and SEO. Usually, you can just click the Generate button and all will be fine.',
       options: {
-        source: doc => `${doc.category}-${doc.title}`
+        source: (doc) => `${doc.category}-${doc.title}`,
       },
-      validation: Rule => Rule.required()
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'hero',
       title: 'Hero',
-      type: 'hero'
+      type: 'hero',
     },
     {
-      name: 'bodyText',
+      name: 'story',
       title: 'Story',
-      type: 'richTextMedia'
+      description: 'Formatted text content and inline pods for a more directed content experienve.',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Normal', value: 'normal' },
+            { title: 'Heading 1', value: 'h1' },
+            { title: 'Heading 2', value: 'h2' },
+            { title: 'Heading 3', value: 'h3' },
+            { title: 'Blockquote', value: 'blockquote' },
+          ],
+          marks: {
+            decorators: [
+              { title: 'Bold', value: 'strong' },
+              { title: 'Emphasis', value: 'em' },
+            ],
+          },
+        },
+        {
+          type: 'hero',
+          title: 'Hero Image',
+        },
+        {
+          type: 'annotatedImage',
+          title: 'Content Image',
+        },
+        {
+          type: 'callout',
+          title: 'Callout',
+        },
+      ],
     },
     {
       name: 'pods',
@@ -91,10 +123,10 @@ export default {
             { type: 'royalsPerson' },
             { type: 'royalsPeopleCollection' },
             { type: 'royalsPlace' },
-            { type: 'imagePod' }
-          ]
-        }
-      ]
-    }
-  ]
+            { type: 'imagePod' },
+          ],
+        },
+      ],
+    },
+  ],
 };
